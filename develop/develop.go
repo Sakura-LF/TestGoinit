a1cb8838467f4b5a7340f37db6dca0d1c16ad145
SakurasZ <80140609+Sakura-LF@users.noreply.github.com>
2024-08-02 18:03:41 +0800

    Update develop.go

--- develop/develop.go
+++ develop/develop.go
@@ -5,7 +5,7 @@ import "fmt"
 // 开发中的代码,继续开发
 func main() {
 	fmt.Println("develop 分支")
-	fmt.Println("Feature1完成")
+	fmt.Println("Feature1没有完成")
 	fmt.Println("继续开发啊啊啊啊啊啊啊")
 	fmt.Println("继续开发开发开发开发开发")
 }
